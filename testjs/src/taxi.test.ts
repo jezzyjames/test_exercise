25d5a819e80d6da85d210d9b38551b87df2305eb
Tasit.s <tasit.s@arise.tech>
2024-05-15 16:26:57 +0700

    [Jet] TestFizzBuzzShouldReturn1WhenInput1

--- testjs/src/taxi.test.ts
+++ testjs/src/taxi.test.ts
@@ -1,8 +1,50 @@
-import {Taxi} from './taxi'
+import { CalculateFare, Taxi } from "./taxi";
 
-describe('Taxi', () => {
-  it('example test', () => {
-    const result = Taxi()
-    expect(result).toEqual(55)
-  })
-})
+describe("Taxi", () => {
+  test("Should return 0 when km and minute are 0", () => {
+    const km = 0;
+    const minute = 0;
+
+    const result = CalculateFare(km, minute);
+
+    expect(result).toEqual(0);
+  });
+
+  test("Should return 5 when km and minute are 1", () => {
+    const km = 1;
+    const minute = 1;
+
+    const result = CalculateFare(km, minute);
+
+    expect(result).toEqual(5);
+  });
+
+  test("Should return 52 when km is 10.1 and minute is 10", () => {
+    const km = 10.1;
+    const minute = 10;
+
+    const result = CalculateFare(km, minute);
+
+    expect(result).toEqual(52);
+  });
+
+  test("Should return 48 when km is 10.1 and minute is 5.5", () => {
+    const km = 10.1;
+    const minute = 5.5;
+
+    const result = CalculateFare(km, minute);
+
+    expect(result).toEqual(48);
+  });
+
+  test("Should return 50 when km is 10.7 and minute is 5.5", () => {
+    const km = 10.7;
+    const minute = 5.5;
+
+    const result = CalculateFare(km, minute);
+
+    expect(result).toEqual(50);
+  });
+});
+
+// 4 * km + 1 * minute
--- testjs/src/taxi.ts
+++ testjs/src/taxi.ts
@@ -1,1 +1,10 @@
-export const Taxi = () => 55
+export const Taxi = () => 55;
+
+export function CalculateFare(km: number, minute: number): number {
+  const roundedKm = Math.ceil(km * 2) / 2;
+  const roundedMinute = Math.ceil(minute);
+
+  const result = 4 * roundedKm + 1 * roundedMinute;
+
+  return result;
+}
