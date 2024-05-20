c18a4edfac3a2139ac9b4fc95c7fd35c9ce3377f
Tasit.s <tasit.s@arise.tech>
2024-05-20 09:29:37 +0700

    [Jet] Round 2, 1 IF (option 2)

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"sort"
-	"strconv"
-)
-
 func FizzBuzz(n int) string {
-	fizzBuzzMap := map[int]string{
-		3:  "Fizz",
-		5:  "Buzz",
-		15: "FizzBuzz",
-	}
-
-	keys := []int{}
-	for k := range fizzBuzzMap {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-
-	value := strconv.Itoa(n)
-
-	for _, key := range keys {
-		if n%key == 0 {
-			value = fizzBuzzMap[key]
-		}
-	}
-
-	return value
+	return "1"
 }
