cdc7bd630f70c533bfb4e2712e5534228eaa5120
Tasit.s <tasit.s@arise.tech>
2024-05-20 09:52:00 +0700

    TestFizzBuzzShouldReturn11WhenInput11

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -114,3 +114,14 @@ func TestFizzBuzzShouldReturnBuzzWhenInput10(t *testing.T) {
 	}
 }
 
+func TestFizzBuzzShouldReturn11WhenInput11(t *testing.T) {
+	input := 11
+
+	got := FizzBuzz(input)
+
+	want := "11"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
+
