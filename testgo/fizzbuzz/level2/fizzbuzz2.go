950eab36b4b4c2923583baf5804f26e21c86d867
Tasit.s <tasit.s@arise.tech>
2024-05-23 20:36:23 +0700

    refactored folder structure

---
+++ testgo/fizzbuzz/level2/fizzbuzz2.go
@@ -0,0 +1,30 @@
+package level2
+
+import (
+	"sort"
+	"strconv"
+)
+
+func FizzBuzz(n int) string {
+
+	fizzBuzzMap := map[int]string{
+		3:  "Fizz",
+		5:  "Buzz",
+		15: "FizzBuzz",
+	}
+
+	keys := []int{}
+	for key := range fizzBuzzMap {
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+
+	result := strconv.Itoa(n)
+	for _, k := range keys {
+		if n%k == 0 {
+			result = fizzBuzzMap[k]
+		}
+	}
+
+	return result
+}
--- testgo/fizzbuzz/level2/fizzbuzz2_test.go
+++ testgo/fizzbuzz/level2/fizzbuzz2_test.go
@@ -1,4 +1,4 @@
-package main
+package level2
 
 import (
 	"testing"
