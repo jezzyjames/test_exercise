1320a6b5b07918dfdf4a32a801ca156d0139a06f
Tasit.s <tasit.s@arise.tech>
2024-05-23 17:29:35 +0700

    refactored code

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -4,31 +4,6 @@ import (
 	"strconv"
 )
 
-// // 1 IF
-// func FizzBuzz(n int) string {
-
-// 	fizzBuzzMap := map[int]string{
-// 		3:  "Fizz",
-// 		5:  "Buzz",
-// 		15: "FizzBuzz",
-// 	}
-
-// 	keys := []int{}
-// 	for key := range fizzBuzzMap {
-// 		keys = append(keys, key)
-// 	}
-// 	sort.Ints(keys)
-
-// 	result := strconv.Itoa(n)
-// 	for _, k := range keys {
-// 		if n%k == 0 {
-// 			result = fizzBuzzMap[k]
-// 		}
-// 	}
-
-// 	return result
-// }
-
 // No IF
 func FizzBuzz(n int) string {
 
@@ -61,3 +36,28 @@ func FizzBuzz(n int) string {
 
 	return fbResult[string(fb)]
 }
+
+// // 1 IF
+// func FizzBuzz(n int) string {
+
+// 	fizzBuzzMap := map[int]string{
+// 		3:  "Fizz",
+// 		5:  "Buzz",
+// 		15: "FizzBuzz",
+// 	}
+
+// 	keys := []int{}
+// 	for key := range fizzBuzzMap {
+// 		keys = append(keys, key)
+// 	}
+// 	sort.Ints(keys)
+
+// 	result := strconv.Itoa(n)
+// 	for _, k := range keys {
+// 		if n%k == 0 {
+// 			result = fizzBuzzMap[k]
+// 		}
+// 	}
+
+// 	return result
+// }
