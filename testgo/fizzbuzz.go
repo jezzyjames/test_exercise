365cdd4636d96754b333797bb00e1e97976749cb
Tasit.s <tasit.s@arise.tech>
2024-05-23 15:10:26 +0700

    No IF option, TestFizzBuzzShouldReturn1WhenInput1

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,30 +1,31 @@
 package main
 
-import (
-	"sort"
-	"strconv"
-)
+// // 1 IF
+// func FizzBuzz(n int) string {
 
+// 	fizzBuzzMap := map[int]string{
+// 		3:  "Fizz",
+// 		5:  "Buzz",
+// 		15: "FizzBuzz",
+// 	}
+
+// 	keys := []int{}
+// 	for key := range fizzBuzzMap {
+// 		keys = append(keys, key)
+// 	}
+// 	sort.Ints(keys)
+
+// 	result := strconv.Itoa(n)
+// 	for _, k := range keys {
+// 		if n%k == 0 {
+// 			result = fizzBuzzMap[k]
+// 		}
+// 	}
+
+// 	return result
+// }
+
+// No IF
 func FizzBuzz(n int) string {
-
-	fizzBuzzMap := map[int]string{
-		3:  "Fizz",
-		5:  "Buzz",
-		15: "FizzBuzz",
-	}
-
-	keys := []int{}
-	for key := range fizzBuzzMap {
-		keys = append(keys, key)
-	}
-	sort.Ints(keys)
-
-	result := strconv.Itoa(n)
-	for _, k := range keys {
-		if n%k == 0 {
-			result = fizzBuzzMap[k]
-		}
-	}
-
-	return result
+	return "1"
 }
